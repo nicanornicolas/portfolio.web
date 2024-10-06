9ea4a1460e254e87d376360c4f4ee0653f9e253f
Masiza-Fidel <fidelmasitsa03@gmail.com>
2024-10-06 06:37:41 +0200

    mama

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ function headerShadow() {
 
 /* ----- TYPING EFFECT ----- */
 var typingEffect = new Typed(".typedText",{
-  strings : ["Software Dev","Enterprenuer",],
+  strings : ["A Software Developer","An Enterprenuer",],
   loop : true,
   typeSpeed : 100, 
   backSpeed : 80,
